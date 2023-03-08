244aacac848e4877bf116bc8ae8ca78731910adb
chai <chaiweimin1902@gmail.com>
2023-03-08 11:26:54 +0800

    Commit on 22/2/2023 First push -Added textview -Added button -Added logic -Add dice image

--- app/src/main/java/com/example/group356practicalw1/MainActivity.kt
+++ app/src/main/java/com/example/group356practicalw1/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.group356practicalw1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.diceImage)
     }
         /*
         Click Listener for the Roll Button
@@ -28,6 +33,19 @@ class MainActivity : AppCompatActivity() {
         val randomInt = (1..6).random()
 
         val resultText: TextView = findViewById(R.id.txtNumber)
+
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+
+        }
+
+        diceImage.setImageResource(drawableResource)
 
         resultText.text = randomInt.toString()
     }
